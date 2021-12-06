a9b9765b7d4efba8087dba2a2464cb86e1022898
MohamedAli <mohamedali4080@gmail.com>
2021-12-06 23:23:31 +0300

    add summery action and ui

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Rocy.Data;
 using Rocy.Models;
+using Rocy.Models.ViewModels;
 using Rocy.Utility;
 
 namespace Rocy.Controllers
@@ -13,6 +15,8 @@ namespace Rocy.Controllers
     [Authorize]
     public class CartController : Controller
     {
+        [BindProperty]
+        ProductUserVm productuserVm { get; set; }
         private readonly ApplicationDbContext _db;
         public CartController(ApplicationDbContext db)
         {
@@ -22,28 +26,61 @@ namespace Rocy.Controllers
 
         public IActionResult Index()
         {
-            List<ShoppingCart> shoppingCart=new List<ShoppingCart> ();
-            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConst.SessionCart)!=null
-            && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConst.SessionCart).Count()>0
+            List<ShoppingCart> shoppingCart = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConst.SessionCart) != null
+            && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConst.SessionCart).Count() > 0
             )
             {
-                shoppingCart=HttpContext.Session.Get<List<ShoppingCart>>(AppConst.SessionCart);    
+                shoppingCart = HttpContext.Session.Get<List<ShoppingCart>>(AppConst.SessionCart);
             }
-                List<int> prodIds=shoppingCart.Select(p=>p.ProductId).ToList();
-                var products=_db.product.Where(p=>prodIds.Contains(p.Id));
+            List<int> prodIds = shoppingCart.Select(p => p.ProductId).ToList();
+            var products = _db.product.Where(p => prodIds.Contains(p.Id));
 
 
             return View(products);
         }
 
-        public IActionResult Remove(int Id){
 
-             List<ShoppingCart> shoppingCart=new List<ShoppingCart> ();
-             shoppingCart=HttpContext.Session.Get<List<ShoppingCart>>(AppConst.SessionCart);
+        [HttpPost()]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult Indexpost()
+        {
 
-             var RemoveProduct=shoppingCart.Where(s=>s.ProductId==Id).FirstOrDefault();
-             shoppingCart.Remove(RemoveProduct);
-             HttpContext.Session.Set(AppConst.SessionCart,shoppingCart);
+            return RedirectToAction(nameof(Summery));
+        }
+
+        public IActionResult Summery()
+        {
+
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userid = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCart = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConst.SessionCart) != null
+            && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConst.SessionCart).Count() > 0
+            )
+            {
+                shoppingCart = HttpContext.Session.Get<List<ShoppingCart>>(AppConst.SessionCart);
+            }
+            List<int> prodIds = shoppingCart.Select(p => p.ProductId).ToList();
+            var products = _db.product.Where(p => prodIds.Contains(p.Id));
+
+            productuserVm=new ProductUserVm (){
+                User=_db.ApplicationUser.FirstOrDefault(u=>u.Id==claim.Value)
+            };
+            return View(productuserVm);
+        }
+        public IActionResult Remove(int Id)
+        {
+
+            List<ShoppingCart> shoppingCart = new List<ShoppingCart>();
+            shoppingCart = HttpContext.Session.Get<List<ShoppingCart>>(AppConst.SessionCart);
+
+            var RemoveProduct = shoppingCart.Where(s => s.ProductId == Id).FirstOrDefault();
+            shoppingCart.Remove(RemoveProduct);
+            HttpContext.Session.Set(AppConst.SessionCart, shoppingCart);
             return RedirectToAction(nameof(Index));
 
 
